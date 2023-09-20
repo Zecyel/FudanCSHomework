c84d47c9ad46934baeee135d8e8708aa73ca8b8d
Zecyel <zecyel@163.com>
2023-09-20 21:54:19 +0800

    Fix: 修复了hw3/h1.c的输出顺序

--- hw3/h1.c
+++ hw3/h1.c
@@ -13,11 +13,11 @@ void SolveQuadraticEquation(float a, float b, float c, int *numSolutions, float 
         float sol2 = (-b - sqrt(discriminant)) / 2 / a;
         *numSolutions = 2;
         if (sol1 > sol2) {
-            solution[0] = sol1;
-            solution[1] = sol2;
-        } else {
             solution[0] = sol2;
             solution[1] = sol1;
+        } else {
+            solution[0] = sol1;
+            solution[1] = sol2;
         }
     }
 }
